52c7b1d71e6ac149a2dcdcbefcea1088280edb38
kindking-dev <islamakhmetov.dev@gmail.com>
2026-09-25 02:13:13 +0500

    Update schedule

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  base: "./",
+  base: "/my-schedule/",
   plugins: [react()],
   resolve: {
     alias: {
-      "@chakra-ui/react": fileURLToPath(new URL("./src/map-shims/chakra.jsx", import.meta.url)),
-      "react-zoom-pan-pinch": fileURLToPath(new URL("./src/map-shims/zoom.jsx", import.meta.url))
+      "@chakra-ui/react": fileURLToPath(
+        new URL("./src/map-shims/chakra.jsx", import.meta.url)
+      ),
+      "react-zoom-pan-pinch": fileURLToPath(
+        new URL("./src/map-shims/zoom.jsx", import.meta.url)
+      )
     }
   }
 });
